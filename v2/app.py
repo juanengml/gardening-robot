199428e1019d0788a6df516cb0aefd593c9d4aca
juanengml <juanengml@gmail.com>
2019-07-01 22:47:22 -0300

    Update new garden iot

--- v2/app.py
+++ v2/app.py
@@ -7,52 +7,19 @@ import sys
 import paho.mqtt.client as mqtt
 import json
 import ast
+from os import system
 
 try:
  ser = Serial("/dev/ttyUSB0",9600)
 except:
  ser = Serial("/dev/ttyUSB1",9600)
 
+#THINGSBOARD_HOST = 'delrey.td.utfpr.edu.br'
+#ACCESS_TOKEN = 'jxvHtpYfVmfUOSuefstq'
 THINGSBOARD_HOST = 'delrey.td.utfpr.edu.br'
-ACCESS_TOKEN = 'jxvHtpYfVmfUOSuefstq'
+ACCESS_TOKEN = 'vHXgIJCOkDqcuuJQwjd0'
 INTERVAL=2
-#import model
 
-"""
-gpio_state = {1: False, 2: False}
-
-def on_connect(client, userdata, rc, *extra_params):
-    print('Connected with result code ' + str(rc))
-    client.subscribe('v1/devices/me/rpc/request/+')
-    client.publish('v1/devices/me/attributes', get_gpio_status(), 1)
-
-def on_message(client, userdata, msg):
-    print 'Topic: ' + msg.topic + '\nMessage: ' + str(msg.payload)
-    data = json.loads(msg.payload)
-    if data['method'] == 'getGpioStatus':
-        client.publish(msg.topic.replace('request', 'response'), get_gpio_status(), 1)
-    elif data['method'] == 'setGpioStatus':
-        set_gpio_status(data['params']['pin'], data['params']['enabled'])
-        client.publish(msg.topic.replace('request', 'response'), get_gpio_status(), 1)
-        client.publish('v1/devices/me/attributes', get_gpio_status(), 1)
-
-
-def get_gpio_status():
-    return json.dumps(gpio_state)
-
-
-def set_gpio_status(pin, status):
-    print pin,status
-    if pin == 1 and status == True:
-        ser.write("w")
-    if pin == 1 and status == False:
-        ser.write("q")
-    if pin == 2 and status == True:
-        ser.write("r")
-    if pin == 2 and status == False:
-        ser.write("e")
-    gpio_state[pin] = status
-"""
 def tratamento(msg):
     dicionario = ast.literal_eval(str(msg.split("\r\n")[0]))
     try:
@@ -84,6 +51,8 @@ try:
         sensor_data = tratamento(text)
         #preditor(text)
         print json.dumps(sensor_data)
+        
+        system("echo %s >> data.csv" %  json.dumps(sensor_data))
 
         client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
 
